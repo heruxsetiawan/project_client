fde0a693ed72a4a47b40a938c9b8f397d02b3efd
heruxsetiawan <a111106098@gmail.com>
2017-10-12 13:42:24 +0700

    tes123

--- app/src/main/java/com/example/united/mrk/MainActivity.java
+++ app/src/main/java/com/example/united/mrk/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getIDs();
+        //tes123
         String menuJson = CreateFileJson.getData(getApplicationContext(), "menu");
         if (menuJson == null) {
             fragmentParent.GetDataJsonfilter("menu", "stadion");
